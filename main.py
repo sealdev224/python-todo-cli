4d44578a84a166d7a4b474690e8066e2e88a246d
sealdev224 <sealdev224@gmail.com>
2026-05-19 15:59:28 -0500

    mehr

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import json
-from colorama import Fore, Style
 
 class Task:
     def __init__(self, task, done=False):
@@ -14,6 +13,7 @@ class Task:
 try:
     with open("user_data.json", "r") as file:
         data = json.load(file)
+    user_data = data
 except FileNotFoundError:
  
     print("What is your name?")
@@ -27,11 +27,37 @@ except FileNotFoundError:
     }
  
     with open("user_data.json", "w") as file:
-        json.dump(user_data, file)
+        json.dump(user_data, file, indent=4)
 except:
     print("Error...???")
 
 running = True
 
-print("Create a task")
-input("Name: ")
+def tCreation():
+    task_name = input("Task Name: ")
+    _task = Task(task_name)
+
+    user_data["todos"].append(
+        {
+            "task": _task.task,
+            "done": _task.done
+        }
+    )
+
+    with open("user_data.json", "w") as file:
+        json.dump(user_data, file, indent=4)
+        
+def listTask():
+    for i in user_data["todos"]:
+        print(i)
+        
+while running:
+    print("1.Create a task\n2.List all task\n3.Clear all completed task\n4.Mark task as completed")
+    input("Enter:")
+    
+    if input == 1:
+        tCreation()
+    if input == 2:
+        listTask()
+    else:
+        print("Unknown...")
